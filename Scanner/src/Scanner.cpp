bee54115a43532db13d70e6259493e4a57318c17
Clemens Basler <clemens-basler@t-online.de>
2017-01-30 18:47:47 +0100

    refactor

--- Scanner/src/Scanner.cpp
+++ Scanner/src/Scanner.cpp
@@ -121,67 +121,72 @@ Token* token;
 
 	} while (automat_result != 0 && automat_result != 20 && automat_result != -1 && automat_result != -99);
 
-								switch (automat_result) {
-								case 0: {
-																//TODO: Comment why we do this
-																if (c != '\n' && c != ' ') {
-																								counter--;
-																}
+switch (automat_result) {
 
-																write(filedesc, "Token ", 6);
-																write(filedesc, translateType(automat->gettype()), 15);
-																write(filedesc, " Line: ", 7);
-																writeInt(automat->getline(), filedesc);
-																write(filedesc, " Column: ", 9);
-																writeInt(automat->getcolumn() - counter, filedesc);
-																buffer->stepBack(1);
+case 0: {
 
-																// Integer
-																if (automat->gettype() == 2) {
+if (c != '\n' && c != ' ') {
+	counter--;
+}
 
-																								buffer->stepBack(counter);
-																								char number[counter];
-																								for (size_t i = 0; i < counter; i++) {
-																																number[i] = buffer->getChar();
-																								}
-																								long value = strtol(number, NULL, 10);
-																								if (errno == ERANGE) {
-																																error(0, ERANGE, "LINE: %d Column: %d", automat->getline(), automat->getcolumn() - counter);
-																																errno = 0;
-																																// TODO: CHECK WHAT TYPE ERROR IS
-																																token = new Token(Token::ERROR, automat->getline(), automat->getcolumn() - counter);
-																								}
-																								token = new Token(Token::INTEGER, automat->getline(), automat->getcolumn() - counter, value);
-																								write(filedesc, " ", 1);
-																								write(filedesc, number, counter);
-																}else{
-																// Alles neben Integer
-																	buffer->stepBack(counter);
-																	char word[counter + 1];
-																	for (size_t i = 0; i < counter; i++) {
-																									word[i] = buffer->getChar();
-																	}
-																	word[counter] = '\0';
-																	Information* myInformation = new Information(word);
+write(filedesc, "Token ", 6);
+write(filedesc, translateType(automat->gettype()), 15);
+write(filedesc, " Line: ", 7);
+writeInt(automat->getline(), filedesc);
+write(filedesc, " Column: ", 9);
+writeInt(automat->getcolumn() - counter, filedesc);
+buffer->stepBack(1);
+// Integer
+if (automat->gettype() == 2) {
 
-																	// Identifier => in Symboltabelle eintragen
-																	if (automat->gettype() == 1) {
-																		myInformation->setType(Token::IDENTIFIER);
-																	}
+	buffer->stepBack(counter);
+	char number[counter];
 
-																	Key* myKey = symTable->insert(myInformation);
-																	Information* info = symTable->lookup(myKey);
-																	// Get Token Type from symTable --> information
-																	token = new Token(info->getType(), automat->getline(), automat->getcolumn() - counter, info);
-																	write(filedesc, " ", 1);
-																	write(filedesc, word, counter);
+	for (size_t i = 0; i < counter; i++) {
+		number[i] = buffer->getChar();
+	}
 
-																}
+long value = strtol(number, NULL, 10);
 
-																write(filedesc, "\n", 1);
-																counter = 0;
-								}
-								break;
+if (errno == ERANGE) {
+	error(0, ERANGE, "LINE: %d Column: %d", automat->getline(), automat->getcolumn() - counter);
+	errno = 0;
+	token = new Token(Token::ERROR, automat->getline(), automat->getcolumn() - counter);
+}
+
+token = new Token(Token::INTEGER, automat->getline(), automat->getcolumn() - counter, value);
+write(filedesc, " ", 1);
+write(filedesc, number, counter);
+
+}else{
+// Alles neben Integer
+	buffer->stepBack(counter);
+	char word[counter + 1];
+
+	for (size_t i = 0; i < counter; i++) {
+		word[i] = buffer->getChar();
+	}
+
+	word[counter] = '\0';
+	Information* myInformation = new Information(word);
+	// Identifier => in Symboltabelle eintragen
+	if (automat->gettype() == 1) {
+		myInformation->setType(Token::IDENTIFIER);
+	}
+
+	Key* myKey = symTable->insert(myInformation);
+	Information* info = symTable->lookup(myKey);
+	// Get Token Type from symTable --> information
+	token = new Token(info->getType(), automat->getline(), automat->getcolumn() - counter, info);
+	write(filedesc, " ", 1);
+	write(filedesc, word, counter);
+}
+
+write(filedesc, "\n", 1);
+counter = 0;
+
+}
+break;
 								case 20: {
 																// write chars into token....
 																buffer->stepBack(counter);
